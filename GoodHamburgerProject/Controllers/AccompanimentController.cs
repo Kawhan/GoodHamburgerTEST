a0146d80a7663c2d916d17604c6c33feb774ea6e
kawhan <kawhanlaurindo@outlook.com>
2026-04-23 11:11:55 -0300

    Finished accompaniment api doc

--- GoodHamburgerProject/Controllers/AccompanimentController.cs
+++ GoodHamburgerProject/Controllers/AccompanimentController.cs
@@ -9,25 +9,55 @@ namespace GoodHamburgerProject.Controllers
     [ApiController]
     public class AccompanimentController(IAccompanimentService serviceAccompaniment) : ControllerBase
     {
+
+        /// <summary>
+        /// Retrieves all accompaniments.
+        /// </summary>
+        /// <returns>A list of accompaniments</returns>
+        /// <response code="200">Returns the list of accompaniments</response>
+        /// <response code="500">Database Error</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<AccompanimentResponseDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpGet]
         public async Task<ActionResult<List<BurgerResponseDTO>>> GetAllAccompaniments()
         {
             return Ok(await serviceAccompaniment.GetAllAccompanimentsAsync());
         }
 
+
+        /// <summary>
+        /// Retrieves an accompaniment by its unique identifier.
+        /// </summary>
+        /// <param name="id">Accompaniment unique identifier (GUID)</param>
+        /// <returns>An accompaniment if found</returns>
+        /// <response code="200">Returns the accompaniment</response>
+        /// <response code="404">Accompaniment not found</response>
+        /// <response code="500">Database Error</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(AccompanimentResponseDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<AccompanimentResponseDTO>> GetAccompanimentByGuid(Guid id)
         {
             var accompaniment = await serviceAccompaniment.GetAccompanimentByIdAsync(id);
-
-            if (accompaniment is null)
-            {
-                return NotFound("No accompaniment were found with that ID.");
-            }
-
             return Ok(accompaniment);
         }
 
+        /// <summary>
+        /// Creates a new accompaniment.
+        /// </summary>
+        /// <param name="request">Accompaniment data to be created</param>
+        /// <returns>The created accompaniment</returns>
+        /// <response code="201">Accompaniment successfully created</response>
+        /// <response code="400">Invalid request data</response>
+        /// <response code="409">Accompaniment already exists</response>
+        /// <response code="500">Database Error</response>
+        [HttpPost]
+        [ProducesResponseType(typeof(AccompanimentResponseDTO), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost]
         public async Task<ActionResult<AccompanimentResponseDTO>> AddAccompaniment(CreateAccompanimentRequestDTO request)
         {
@@ -39,6 +69,21 @@ namespace GoodHamburgerProject.Controllers
             );
         }
 
+        /// <summary>
+        /// Updates an existing accompaniment.
+        /// </summary>
+        /// <param name="id">Accompaniment unique identifier</param>
+        /// <param name="request">Updated accompaniment data</param>
+        /// <returns>No content if update is successful</returns>
+        /// <response code="204">Accompaniment successfully updated</response>
+        /// <response code="404">Accompaniment not found</response>
+        /// <response code="409">Accompaniment already exists</response>
+        /// <response code="500">Database Error</response>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPut]
         public async Task<ActionResult> UpdateAccompaniment(Guid id, UpdateAccompanimentRequestDTO request)
         {
@@ -46,7 +91,19 @@ namespace GoodHamburgerProject.Controllers
             return updated ? NoContent() : NotFound("No accompaniment were found with that ID.");
         }
 
+
+        /// <summary>
+        /// Deletes (deactivates) an accompaniment by its ID.
+        /// </summary>
+        /// <param name="id">Accompaniment unique identifier</param>
+        /// <returns>No content if deletion is successful</returns>
+        /// <response code="204">Accompaniment successfully deleted</response>
+        /// <response code="404">Accompaniment not found</response>
+        /// <response code="500">Database Error</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> DeleteAccompaniment(Guid id)
         {
             var deleted = await serviceAccompaniment.DeleteAccompanimentAsync(id);
